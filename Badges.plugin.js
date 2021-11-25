f684a8b61e3332ce0465fff5d5964429527037b2
Din <73368499+20cmDuro@users.noreply.github.com>
2021-11-25 16:57:08 +0000

    Add files via upload

---
+++ Badges.plugin.js
@@ -0,0 +1,23 @@
+/**
+ * @name Badges
+ * @version 0.0.1
+ * @description This plugin will give u all the discord badges avaliable!
+ * @author Din#3719
+ *  
+ */
+
+
+
+ module.exports = class Example{
+     
+    load() { }
+    start() {
+
+window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().flags = -1;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().flags = -1}}}]);window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().public_flags += 1;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().public_flags += 1}}}]);
+BdApi.showToast("Badges ativadas com sucesso! | Client-Sided");
+
+}
+stop(){
+
+}
+}
